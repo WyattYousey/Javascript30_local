a2dbeaadb14dc55cde9f5cad97580a683e1f69cf
wyattYousey <quiz.nv1@gmail.com>
2026-03-04 09:42:50 -0600

    Implemented a countdown timer
    This was a really simple project that taught me how you can use the browser provided date and time so you can create your own timer.

---
+++ 29 - Countdown Timer/scripts-START.js
@@ -0,0 +1,53 @@
+let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time');
+
+function timer(seconds) {
+    clearInterval(countdown);
+
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    displayTimeLeft(seconds);
+    displayEndTime(then);
+
+    countdown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+
+        if (secondsLeft < 0) {
+            clearInterval(countdown);
+            return;
+        }
+
+        displayTimeLeft(secondsLeft);
+    }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+    const mins = Math.floor(seconds / 60);
+    const remainderSecs = seconds % 60;
+    const display = `${mins}:${remainderSecs < 10 ? 0 : ''}${remainderSecs}`;
+    document.title = display;
+    timerDisplay.textContent = display;
+}
+
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const adjustedHour = hour > 12 ? hour - 12 : hour;
+    const minutes = end.getMinutes();
+    endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
+}
+
+function startTimer() {
+    const seconds = this.dataset.time;
+    timer(seconds);
+}
+
+buttons.forEach((button) => button.addEventListener('click', startTimer));
+document.customForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const mins = this.minutes.value;
+    timer(mins * 60);
+    this.reset();
+});
